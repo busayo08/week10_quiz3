8b4d185d03fa5fd313ff40c28afc69d387086b8d
busayo08 <43090736+busayo08@users.noreply.github.com>
2018-11-23 04:12:43 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 $('#add').click(
 function(){
+    if (textfield.value == ""){
+    return false;           
+    } else {
     var value = $('#textfield').val();
     $('#list').append('<li>'+value+'</li>');
+    }
 }
 )
